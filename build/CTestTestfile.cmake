7c59c24ac0e0bd1ebcefe5615bdab14a2274ff60
Bricktheworld <brandon.shihabi@gmail.com>
2021-02-12 19:55:12 -0800

    gitignore issue fixed maybe?

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brandon/Dev/GameEngine
-# Build directory: /home/brandon/Dev/GameEngine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
