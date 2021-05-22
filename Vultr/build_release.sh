db64ac3ce5a7621eb357189a65fb108707c510b6
Bricktheworld <brandon.shihabi@gmail.com>
2021-05-21 22:11:30 -0700

    Added release builds

--- Vultr/build_release.sh
+++ Vultr/build_release.sh
@@ -1,2 +1,3 @@
-premake5 gmake
-bear -- make config=release -j10
+/usr/bin/cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/bin/clang-11 -DCMAKE_CXX_COMPILER:FILEPATH=/bin/clang++ -H/home/brandon/Dev/GameEngine/Vultr -B/home/brandon/Dev/GameEngine/Vultr/release -G "Unix Makefiles"
+
+/usr/bin/cmake --build /home/brandon/Dev/GameEngine/Vultr/release --config RelWithDebInfo --target all -- -j 10
--- Vultr/src/helpers/path.cpp
+++ Vultr/src/helpers/path.cpp
@@ -48,29 +48,29 @@ namespace Vultr::Path
 
     std::string GetFullPath(const std::string &path)
     {
-#ifdef ENABLE_DEBUG_MACRO
+// #ifdef ENABLE_DEBUG_MACRO
 #ifdef _WIN32
         return GetWindowsPath(get_resource_path() + path);
 #else
         return get_resource_path() + path;
 #endif
-#else
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-        char buff[256];
-        DWORD len = GetModuleFileName(NULL, buff, sizeof(buff));
-#else
-        char buff[PATH_MAX];
-        ssize_t len = readlink("/proc/self/exe", buff, sizeof(buff) - 1);
-#endif
-        if (len != -1)
-        {
-            buff[len] = '\0';
-            std::string base = std::string(buff);
-            std::string full_path = base.replace(base.rfind("Game"), len, "") + path;
-            return full_path;
-        }
-        assert(false && "COULD NOT GET EXE PATH");
-        return std::string("ERROR");
-#endif
+        // #else
+        // #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+        //         char buff[256];
+        //         DWORD len = GetModuleFileName(NULL, buff, sizeof(buff));
+        // #else
+        //         char buff[PATH_MAX];
+        //         ssize_t len = readlink("/proc/self/exe", buff, sizeof(buff) - 1);
+        // #endif
+        //         if (len != -1)
+        //         {
+        //             buff[len] = '\0';
+        //             std::string base = std::string(buff);
+        //             std::string full_path = base.replace(base.rfind("Game"), len, "") + path;
+        //             return full_path;
+        //         }
+        //         assert(false && "COULD NOT GET EXE PATH");
+        //         return std::string("ERROR");
+        // #endif
     }
 } // namespace Vultr::Path
