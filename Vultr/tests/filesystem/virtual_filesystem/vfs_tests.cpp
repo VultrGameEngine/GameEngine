1f712950296bd4a85ec0af4fe0475e8af4270e59
Bricktheworld <brandon.shihabi@gmail.com>
2021-09-16 00:42:45 -0700

    Added initial vfs test

---
+++ Vultr/tests/filesystem/virtual_filesystem/vfs_tests.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#define private public
+#define protected public
+
+#include <filesystem/virtual_filesystem.h>
+
+using namespace Vultr;
+
+TEST(VirtualFilesystem, Init)
+{
+}
