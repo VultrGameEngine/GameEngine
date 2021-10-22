255e03b5781cd8ca442a9294f7a0f6be924e4594
Bricktheworld <brandon.shihabi@gmail.com>
2021-10-22 01:30:17 -0700

    Rewriting dynamic array so that I can use it later

--- VTL/include/dynamic_array.h
+++ VTL/include/dynamic_array.h
@@ -31,14 +31,111 @@ namespace vtl
             {
                 internal_array[i] = array[i];
             }
-            used = count;
+            len = count;
         }
+
+        // Delete copy methods because we don't want this to be done on accident, we want it to be very very explicit since we are essentially duplicating a buffer
+        DynamicArray<T> &operator=(const DynamicArray<T> &other) = delete;
+        DynamicArray(const DynamicArray<T> &other) = delete;
 
         // Destructor for dynamic array
         ~DynamicArray()
         {
             if (internal_array != nullptr)
                 free(internal_array);
+        }
+
+        // Push element to back of dynamic_array
+        // If the dynamic_array does not have enough space, then a reallocation will
+        // occur
+        //
+        // Returns the element just inserted
+        T *push_back(const T &element)
+        {
+            // If there is not enough space in the dynamic_array, then reallocate
+            if (len >= size)
+            {
+                reallocate(size + 1);
+            }
+
+            // Increase the len amount and assign the last element of array to the new
+            // element
+            internal_array[len++] = element;
+
+            return &internal_array[len];
+        }
+
+        // Inserts element at specific index in dynamic_array
+        // Shifts all elements at and to the right of that index 1 to the right
+        // If the dynamic_array does not have enough space, then a reallocation will
+        // occur
+        //
+        // Throws index out of bounds error if index is greater than the
+        // dynamic_array.size or if index is negative
+        //
+        // Returns the element just inserted
+        T *insert(size_t index, T element)
+        {
+            // Fail if the index is greater than the dynamic_array.size or negative
+            assert(index <= size && index >= 0 && "Index out of bounds!");
+
+            // If there is not enough space in the DynamicArray, then reallocate
+            if (len >= size)
+            {
+                reallocate(size + 1);
+            }
+
+            if (size > 1)
+            {
+                // Shift all elements right
+                for (int i = size - 2; i >= index; i--)
+                {
+                    internal_array[i + 1] = internal_array[i];
+                }
+            }
+
+            // Increase the len amount
+            len++;
+
+            // Assign last element of array to new element
+            internal_array[index] = element;
+
+            return &internal_array[index];
+        }
+
+        // Delete element at index in dynamic_array
+        // Shifts all elements to the right of that index 1 to the left
+        // If the dynamic_array len equals dynamic_array size, then it will assume
+        // nothing is reserved and a reallocation will occur unless reallocate is
+        // explicitly false
+        T remove(size_t index, bool reallocate = true)
+        {
+            // Fail if the index is greater than the dynamic_array.size or negative
+            assert(index <= size && index >= 0 && "Index out of bounds!");
+
+            // Save the element we are removing for later
+            T element = internal_array[index];
+
+            // Shift elements to the left
+            for (uint i = index + 1; i < size; i++)
+            {
+                internal_array[i - 1] = internal_array[i];
+            }
+
+            // If the len == the size and reallocate is true, reallocate
+            if (len == size && reallocate)
+            {
+                this->reallocate(size - 1);
+            }
+
+            // Decrease len even if no reallocation
+            len--;
+
+            return element;
+        }
+
+        static DynamicArray<T> copy()
+        {
         }
 
         // Ability to index into the array and assign and retreive elements by
@@ -49,15 +146,53 @@ namespace vtl
             return internal_array[index];
         }
 
+        // Spaces len (not bytes)
+        size_t len;
+
+        // Space available (not bytes)
+        size_t size;
+
+        // To make sure that the buffer expansion is geometric
+        f64 growth_factor = 1.3;
+
+      private:
+        void reallocate(size_t new_size)
+        {
+            size = new_size;
+            if (size == 0)
+            {
+                if (internal_array != nullptr)
+                {
+                    free(internal_array);
+                    internal_array = nullptr;
+                }
+            }
+            else
+            {
+                if (internal_array == nullptr)
+                {
+                    internal_array = (T *)malloc(new_size * sizeof(T));
+                }
+                else
+                {
+                    internal_array = (T *)realloc(internal_array, new_size * sizeof(T));
+                }
+            }
+        }
+
+        // The internal array
+        T *internal_array;
+
+        // Dumbass C++ shit
+      public:
         struct Iterator
         {
             typedef std::random_access_iterator_tag IteratorCategory;
             typedef T ValueType;
             typedef ValueType *Pointer;
             typedef ValueType &Reference;
-            Iterator(Pointer p_ptr) : ptr(p_ptr)
-            {
-            }
+
+            Iterator(Pointer p_ptr) : ptr(p_ptr){};
 
             Iterator &operator++()
             {
@@ -123,145 +258,6 @@ namespace vtl
         {
             return Iterator(internal_array + size);
         }
-
-        // The internal array
-        T *internal_array;
-
-        // Spaces used (not bytes)
-        u32 used;
-
-        // Space available (not bytes)
-        u32 size;
-
-        f64 growth_factor = 1.3;
     };
-
-    namespace internal
-    {
-        template <typename T>
-        static void dynamic_array_realloc(DynamicArray<T> &a, const uint new_size)
-        {
-            a.size = new_size;
-            if (a.size == 0)
-            {
-                if (a.internal_array != nullptr)
-                {
-                    free(a.internal_array);
-                    a.internal_array = nullptr;
-                }
-            }
-            else
-            {
-                if (a.internal_array == nullptr)
-                {
-                    a.internal_array = (T *)malloc(new_size * sizeof(T));
-                }
-                else
-                {
-                    a.internal_array = (T *)realloc(a.internal_array, new_size * sizeof(T));
-                }
-            }
-        }
-    } // namespace internal
-
-    // Push element to back of dynamic_array
-    // If the dynamic_array does not have enough space, then a reallocation will
-    // occur
-    //
-    // Returns the element just inserted
-    template <typename T>
-    T dynamic_array_push_back(DynamicArray<T> &a, T element)
-    {
-        // If there is not enough space in the dynamic_array, then reallocate
-        if (a.used >= a.size)
-        {
-            internal::dynamic_array_realloc(a, a.size + 1);
-        }
-
-        // Increase the used amount and assign the last element of array to the new
-        // element
-        a.internal_array[a.used++] = element;
-
-        return element;
-    }
-
-    // Inserts element at specific index in dynamic_array
-    // Shifts all elements at and to the right of that index 1 to the right
-    // If the dynamic_array does not have enough space, then a reallocation will
-    // occur
-    //
-    // Throws index out of bounds error if index is greater than the
-    // dynamic_array.size or if index is negative
-    //
-    // Returns the element just inserted
-    template <typename T>
-    T dynamic_array_insert(DynamicArray<T> &a, int index, T element)
-    {
-        // Fail if the index is greater than the dynamic_array.size or negative
-        assert(index <= a.size && index >= 0 && "Index out of bounds!");
-
-        // If there is not enough space in the DynamicArray, then reallocate
-        if (a.used >= a.size)
-        {
-            internal::dynamic_array_realloc(a, a.size + 1);
-        }
-
-        if (a.size > 1)
-        {
-            // Shift all elements right
-            for (int i = a.size - 2; i >= index; i--)
-            {
-                a.internal_array[i + 1] = a.internal_array[i];
-            }
-        }
-
-        // Increase the used amount
-        a.used++;
-
-        // Assign last element of array to new element
-        a.internal_array[index] = element;
-
-        return element;
-    }
-
-    // Delete element at index in dynamic_array
-    // Shifts all elements to the right of that index 1 to the left
-    // If the dynamic_array used equals dynamic_array size, then it will assume
-    // nothing is reserved and a reallocation will occur unless reallocate is
-    // explicitly false
-    template <typename T>
-    T dynamic_array_delete_element(DynamicArray<T> &a, uint index, bool reallocate = true)
-    {
-        // Fail if the index is greater than the dynamic_array.size or negative
-        assert(index <= a.size && index >= 0 && "Index out of bounds!");
-
-        // Save the element we are removing for later
-        T element = a[index];
-
-        // Shift elements to the left
-        for (uint i = index + 1; i < a.size; i++)
-        {
-            a.internal_array[i - 1] = a.internal_array[i];
-        }
-
-        // If the used == the size and reallocate is true, reallocate
-        if (a.used == a.size && reallocate)
-        {
-            internal::dynamic_array_realloc(a, a.size - 1);
-        }
-
-        // Decrease used even if no reallocation
-        a.used--;
-
-        return element;
-    }
-
-    // Get the size of the array
-    // NOT the number of elements
-    template <typename T>
-    uint sizeof_dynamic_array(const DynamicArray<T> &a)
-    {
-        return a.size;
-    }
 
 } // namespace vtl
